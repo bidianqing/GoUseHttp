d622675827ae0d9b3caabd02f22fad4e38a90b1d
bidianqing <bidianqing@longfor.com>
2024-02-19 09:32:05 +0800

    *

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/bidianqing/gousehttp
 
-go 1.21.6
-
-require github.com/julienschmidt/httprouter v1.3.0
+go 1.22.0
--- main.go
+++ main.go
@@ -2,23 +2,14 @@ package main
 
 import (
 	"net/http"
-
-	"github.com/julienschmidt/httprouter"
 )
 
-func Index(response http.ResponseWriter, request *http.Request, _ httprouter.Params) {
-	response.Header().Add("Content-Type", "application/json")
-	response.Write([]byte(`{"name":"tom"}`))
-}
-
 func main() {
-	// router := httprouter.New()
-	// router.GET("/", Index)
-
-	http.Handle("/", http.FileServer(http.Dir("./wwwroot")))
-	http.HandleFunc("/http", func(response http.ResponseWriter, request *http.Request) {
+	server := http.NewServeMux()
+	server.HandleFunc("GET /", func(response http.ResponseWriter, request *http.Request) {
 		response.Header().Add("Content-Type", "application/json")
 		response.Write([]byte(`{"name":"http"}`))
 	})
-	http.ListenAndServe(":8080", nil)
+
+	http.ListenAndServe(":8080", server)
 }
