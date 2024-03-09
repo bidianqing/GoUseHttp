e3db3e73ef38a2a913731c92019461ad36ce3119
bidianqing <bidianqing@qq.com>
2024-03-09 20:44:37 +0800

    *

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	_ "net"
 	"net/http"
 )
 
 func main() {
-	server := http.NewServeMux()
-	server.HandleFunc("GET /", func(response http.ResponseWriter, request *http.Request) {
+	handler := http.NewServeMux()
+	handler.HandleFunc("GET /", func(response http.ResponseWriter, request *http.Request) {
 		response.Header().Add("Content-Type", "application/json")
 		response.Write([]byte(`{"name":"http"}`))
 	})
 
-	http.ListenAndServe(":8080", server)
+	http.ListenAndServe(":8081", handler)
+
+	// server := http.Server{
+	// 	Addr:    ":8080",
+	// 	Handler: handler,
+	// }
+
+	// l, _ := net.Listen("tcp", ":8080")
+	// server.Serve(l)
 }
